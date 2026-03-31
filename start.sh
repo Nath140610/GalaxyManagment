db6e73c0b754a0a1468e0e68ac2a5cda0ad1da7b
Nath140610 <nathanaellepesant@gmail.com>
2026-03-31 23:28:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,2 @@
 #!/bin/sh
-cd "$(dirname "$0")"
-
-if [ ! -d ".venv" ]; then
-  echo "Création du virtualenv..."
-  python3 -m venv .venv
-fi
-
-. .venv/bin/activate
-
-pip install --upgrade pip
-pip install -r requirements.txt
-
 python bot.py
